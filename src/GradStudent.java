9e4d82859b8b3e71fc30fbcc0a158e5cb4ca6b50
Blessing Thomas <136854151+blessinganime@users.noreply.github.com>
2026-05-02 16:12:12 -0400

    Refactor GradStudent class and add research lab feature

--- src/GradStudent.java
+++ src/GradStudent.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class GradStudent extends Student {
+    private String researchLab;
 
-public class gradStudent {
+   
+    public GradStudent(String name, double gpa, String researchLab) {
+        super(name, gpa); // call parent constructor
+        this.researchLab = researchLab;
+    }
+
+    // Override method
+    @Override
+    public String getStatus() {
+        String baseStatus = super.getStatus();
+        return baseStatus + " They research in the " + researchLab + " lab.";
+    }
 }
